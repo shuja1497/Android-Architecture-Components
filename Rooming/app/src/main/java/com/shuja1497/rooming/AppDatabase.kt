ca2ef757dbc61121d1921e41c35d7050d47e92d0
Shuja Reshi <shujareshi1497@gmail.com>
2018-09-28 22:44:09 +0530

    added migration.

--- Rooming/app/src/main/java/com/shuja1497/rooming/AppDatabase.kt
+++ Rooming/app/src/main/java/com/shuja1497/rooming/AppDatabase.kt
@@ -1,15 +1,24 @@
 package com.shuja1497.rooming
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.migration.Migration
 import android.content.Context
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
         private var INSTANCE : AppDatabase? = null
+
+        private val MIGRATION_1_2 = object : Migration(1, 2){
+
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE User ADD COLUMN location TEXT not null DEFAULT '' ")
+            }
+        }
 
         fun getInstance(context: Context): AppDatabase?{
 
@@ -17,6 +26,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "user.db")
                     .allowMainThreadQueries() // for allowing usage in main thread
+                    .addMigrations(MIGRATION_1_2)
                     .build()
 
             return INSTANCE

a59a95727cc828d05865f424efe8ae07fb7cb456
shuja1497 <muhammad.shuja@magicpin.in>
2020-01-19 23:12:56 +0530

    basic navigation completed

--- Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/view/DetailFragment.kt
+++ Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/view/DetailFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 import sarcastic.cule.jetpacked.R
 
@@ -20,6 +22,15 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonList.setOnClickListener {
+            val action = DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 
--- Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/view/ListFragment.kt
+++ Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/view/ListFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_list.*
 
 import sarcastic.cule.jetpacked.R
 
@@ -20,6 +22,15 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetails.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 

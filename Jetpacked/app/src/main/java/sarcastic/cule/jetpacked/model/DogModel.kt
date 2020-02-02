67a8f3c408a0be27cc2d2d57700aa9cce015b2e7
shuja1497 <shujareshi1497@gmail.com>
2020-02-02 17:34:11 +0530

    setup done for api service

--- Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/model/DogModel.kt
+++ Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/model/DogModel.kt
@@ -1,11 +1,27 @@
 package sarcastic.cule.jetpacked.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
+
+    @SerializedName("id")
     val breedId: String?,
+
+    @SerializedName("name")
     val breed: String,
+
+    @SerializedName("life_span")
     val lifespan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String?,
+
+    @SerializedName("bred_for")
     val bredFor: String?,
+
+    @SerializedName("temperament")
     val temperament: String?,
+
+    @SerializedName("url")
     val imageUrl: String?
 )

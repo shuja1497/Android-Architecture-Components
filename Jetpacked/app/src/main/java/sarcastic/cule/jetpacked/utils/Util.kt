1acc8e8284c5adb6110ddcb320d5c164350175c6
shuja1497 <shujareshi1497@gmail.com>
2020-02-05 01:07:29 +0530

    binding images

--- Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/utils/Util.kt
+++ Jetpacked/app/src/main/java/sarcastic/cule/jetpacked/utils/Util.kt
@@ -2,6 +2,7 @@ package sarcastic.cule.jetpacked.utils
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -25,4 +26,9 @@ fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable
         .setDefaultRequestOptions(options)
         .load(url)
         .into(this)
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
 }
